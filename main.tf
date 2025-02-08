a1c8ec77fa597d3171fcb57b9292588da32d76f7
YZ-Trainer <mantur@gmail.com>
2025-02-08 12:59:34 +0100

    blog_alb from the 04_02 branch

--- main.tf
+++ main.tf
@@ -46,41 +46,40 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "alb" {
-  source = "terraform-aws-modules/alb/aws"
+module "blog_alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "~> 6.0"
 
-  name    = "blog-alb"
-  vpc_id  = module.blog_vpc.vpc_id
-  subnets = module.blog_vpc.public_subnets   
+  name = "blog-alb"
 
-  security_groups = [module.blog_sg.security_group_id]
+  load_balancer_type = "application"
 
-  access_logs = {
-    bucket = "blog-alb-logs"
-  }
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
 
-  listeners = {
-    http-listener = {
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
       port               = 80
       protocol           = "HTTP"
-      target_group_index = 0      
+      target_group_index = 0
     }
- }
-
-  target_groups = {
-    blog-instance = {
-      name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-      target_id        = aws_instance.blog.id
-    }
-  }
+  ]
 
   tags = {
     Environment = "dev"
   }
 }
+
 
 module "blog_sg" {
   source      = "terraform-aws-modules/security-group/aws"
